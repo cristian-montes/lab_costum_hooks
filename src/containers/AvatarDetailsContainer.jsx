22bcda67a9d1bed3eaa747efa5fccd49b7adb03d
Cristian Montes <cmontesmujica@gmail.com>
2021-11-10 18:00:39 -0700

    second costum hook to fetch details working

--- src/containers/AvatarDetailsContainer.jsx
+++ src/containers/AvatarDetailsContainer.jsx
@@ -1,8 +1,30 @@
 import React from "react";
+import { useOneAvatar } from "../hooks/useOneAvatar";
 
 const AvatarDetailsContainer = () => {
+    const {oneAvatar, isLoading} = useOneAvatar();
+
+  console.log(oneAvatar)
+
     return(
-        <h1> THIS IS THE DETAILS PAGE YO!</h1>
+       <div>
+           {
+               isLoading
+                    ?
+                <h1>Loading... give me a minute</h1>
+                    :
+                <div>
+                    <img src={oneAvatar.photoUrl} alt={oneAvatar.name} />
+                    <p>{oneAvatar.name}</p>
+                    <p>allies: {oneAvatar.allies}</p>
+                    <p>affiliation: {oneAvatar.affiliation}</p>
+                    <p>enemies: {oneAvatar.enemies}</p>
+                    <p>profession: {oneAvatar.profession}</p>
+                    <p>skin: {oneAvatar.skin}</p>
+                    <p>weapon: {oneAvatar.weapon}</p>
+                </div>
+           }
+       </div>
     )
 
 }
